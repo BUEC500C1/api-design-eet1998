c1c4df733cc095c377cf2d45d3e5da8f29a216c2
Erin Thomas <erin.thomas@Alan-Turing.local>
2020-02-03 21:42:37 -0500

    Added comments

--- weather_api.py
+++ weather_api.py
@@ -1,11 +1,12 @@
 # Erin Thomas / U26838058 / EC 500: Building Software / Assignment 2
 # Python program to find weather information for any U.S. airport using OpenWeatherMap API
-# References: https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask
+# References: https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask, https://www.geeksforgeeks.org/python-find-current-weather-of-any-city-using-openweathermap-api/
 
 import requests
 import json
 import flask
 
+# Creates Flask application server
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -14,7 +15,7 @@ def home():
     return "<h1>Erin's Weather API</h1><p>This site returns the weather information for a given United States airport.</p>"
 app.run()
 
-
+# Function to call OpenWeaterMap API
 def api_call(city_name):
     base_url = "api.openweathermap.org/data/2.5/weather?"
     complete_url = base_url + "appid=" + api_key + "q=" + city_name
