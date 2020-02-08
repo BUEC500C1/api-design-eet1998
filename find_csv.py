b2e2559a4185525024ea9b8bd890e7d6a9e777ec
Erin Thomas <erin.thomas@Alan-Turing.local>
2020-02-08 18:47:28 -0500

    Updated find_city function

--- find_csv.py
+++ find_csv.py
@@ -1,5 +1,15 @@
+# Reference: https://www.programiz.com/python-programming/reading-csv-files
+
 import csv
 
-filename = "airports.csv"
+# Iterates through all rows in the .csv file and returns city for airport input
+def find_city(airport):
+    with open("airports.csv", 'r') as file:
+        csv_file = csv.DictReader(file)
+        for row in csv_file:
+            if row[3] == airport:
+                return row[10]
 
-def find_city_airport(airport_code):
+    return
+
+
