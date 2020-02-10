462ed01565c29a97d06963cb90e1fbf9892f04b5
Erin Thomas <erin.thomas@Alan-Turing.local>
2020-02-09 20:53:13 -0500

    Fixed reader error

--- find_city.py
+++ find_city.py
@@ -4,7 +4,7 @@ import csv
 # Iterates through all rows in the .csv file and returns city for airport input
 def find_city(airport):
     with open("airports.csv", 'r') as file:
-        csv_file = csv.DictReader(file)
+        csv_file = csv.reader(file)
         for row in csv_file:
             if row[3] == airport:
                 return row[10]
