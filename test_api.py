a8859ae74180127981dd54fbae8e5e9e58d6dcca
Erin Thomas <erin.thomas@dhcp-wifi-8021x-168-122-208-175.bu.edu>
2020-02-10 12:12:33 -0500

    Added bad test case

--- test_api.py
+++ test_api.py
@@ -10,7 +10,11 @@ def test_good_2():
 def test_good_3():
     assert find_city("Spotts Field") == "Nora Springs"
 
+def test_bad():
+    assert find_city("Hello World") == 0
+
 if __name__ == "__main__":
     test_good_1()
     test_good_2()
     test_good_3()
+    test_bad()
